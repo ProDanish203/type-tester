6127f5cba55bc89425eb4864aa8aac2252287743
Danish <danishsidd524@gmail.com>
2025-02-03 23:24:59 +0500

    basic server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello world");
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+// For url inputs
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(morgan("dev"));
+app.disable("x-powered-by");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World", success: true });
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
