fb8154cb0b73da49ff40c92cd8c918dcb6a547e6
Danish <danishsidd524@gmail.com>
2025-02-05 22:48:24 +0500

    add: form handlers

--- client/components/shared/MultiplayerModal.tsx
+++ client/components/shared/MultiplayerModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import {
   Dialog,
   DialogHeader,
@@ -26,6 +26,19 @@ export const MultiplayerModal: React.FC<MultiplayerModalProps> = ({
   open,
   setOpen,
 }) => {
+  const [username, setUsername] = useState("");
+  const [roomId, setRoomId] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleCreateRoom = () => {};
+
   return (
     <Dialog open={open} onOpenChange={setOpen} defaultOpen={false}>
       <DialogTrigger
@@ -47,33 +60,57 @@ export const MultiplayerModal: React.FC<MultiplayerModalProps> = ({
             Join a game with your friends or random players all over the globe.
           </DialogDescription>
         </DialogHeader>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <div className="flex gap-y-3 flex-col justify-center">
             <Label htmlFor="username">Enter Username</Label>
-            <Input id="username" required minLength={1} maxLength={30} />
+            <Input
+              id="username"
+              required
+              value={username}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setUsername(e.target.value)
+              }
+              minLength={1}
+              maxLength={30}
+            />
           </div>
 
           <div className="flex gap-y-3 flex-col justify-center">
             <Label htmlFor="roomId">Enter Room Id</Label>
-            <Input id="roomId" required minLength={1} maxLength={30} />
+            <Input
+              id="roomId"
+              required
+              value={roomId}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setRoomId(e.target.value)
+              }
+              minLength={1}
+              maxLength={30}
+            />
           </div>
 
           <Separator />
           <p className="text-center text-lg">OR</p>
           <div className="flex gap-y-3 flex-col justify-center">
-            <Button variant={"secondary"} className="mb-2">
+            <Button
+              type="button"
+              onClick={handleCreateRoom}
+              variant={"secondary"}
+              className="mb-2"
+            >
               Create New Room
             </Button>
           </div>
 
           <div className="flex items-center justify-end gap-x-2 mt-2">
             <DialogClose
+              type="button"
               className={cn(buttonVariants({ variant: "destructive" }))}
             >
               Cancel
             </DialogClose>
 
-            <Button>Join Room</Button>
+            <Button type="submit">Join Room</Button>
           </div>
         </form>
       </DialogContent>
